71aa9844b8ba09c619b294c608e15c20f4ffa283
midas-myth <midas-myth@proton.me>
2024-06-12 04:28:16 +0400

    chore: Add AVAX Fuji testnet configuration to hardhat.config.ts

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -3,6 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      from: "0x3e46eC396Da353adf31090629e00C04222EBEaf3",
+      //
+      accounts: [],
+    },
+  },
 };
 
 export default config;
